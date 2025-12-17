8ba5a9d1ac0f60929b65c8e9e35f1488d43c018c
ez <samadireza1998@gmail.com>
2025-12-17 19:50:35 +0330

    Uncomment DB configuration in .env file
    Uncomment database configuration variables in .env

--- moon.sh
+++ moon.sh
@@ -105,6 +105,11 @@ install() {
         cp .env.example .env
     fi
 
+    sed -i 's/^# DB_HOST/DB_HOST/' .env
+    sed -i 's/^# DB_PORT/DB_PORT/' .env
+    sed -i 's/^# DB_DATABASE/DB_DATABASE/' .env
+    sed -i 's/^# DB_USERNAME/DB_USERNAME/' .env
+    sed -i 's/^# DB_PASSWORD/DB_PASSWORD/' .env
     sed -i "s/DB_CONNECTION=.*/DB_CONNECTION=mysql/" .env
     sed -i "s/DB_DATABASE=.*/DB_DATABASE=$MAINDB/" .env
     sed -i "s/DB_USERNAME=.*/DB_USERNAME=$DB_USER/" .env
